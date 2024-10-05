ba81ca193967a66184493620eda8ac525fbc8a2f
Omar Gawdat <omargawdaat@gmail.com>
2024-10-05 12:19:41 +0300

    Initial commit

---
+++ src/components/SocialBtns.jsx
@@ -0,0 +1,30 @@
+import { Icon } from '@iconify/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SocialBtns({ variant, socialBtns }) {
+  return (
+      <div
+          className={`social-icon ${variant ? variant : ''}`}
+          data-aos="zoom-in"
+          data-aos-duration="1200"
+          data-aos-delay="300"
+      >
+        {socialBtns?.map((item, index) => (
+            <Link
+                className={item.iconBgClass}
+                to={item.href}
+                key={index}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              {item.customIcon ? (
+                  <img src={item.customIcon} alt={item.iconBgClass} />
+              ) : (
+                  <Icon icon={item.icon} />
+              )}
+            </Link>
+        ))}
+      </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import Brands from '../components/Brands';
+import Projects from '../components/Projects';
+// import Testimonial from '../components/Testimonial';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Service from '../components/Service';
+import HomePagdData from '../data/HomePagdData.json';
+import Experience from '../components/Experience';
+import Contact from '../components/Contact';
+
+export default function Home() {
+    const {
+        hero,
+        socialBtns,
+        brands,
+        about,
+        projects,
+        service,
+        experience,
+        // testimonial,
+        contact,
+    } = HomePagdData;
+    return (
+        <>
+            <Hero data={hero} socialData={socialBtns}/>
+            <Brands data={brands}/>
+            <About data={about}/>
+            <Service data={service}/>
+            <Projects data={projects}/>
+            <Experience data={experience}/>
+            {/*<Testimonial data={testimonial} />*/}
+            <Contact data={contact} socialData={socialBtns}/>
+        </>
+    );
+}
