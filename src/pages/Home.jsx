1ff4072e7fb7d7245a0992ac5bff43876df4e558
omargawdat <omargawdaat@gmail.com>
2025-12-23 21:24:29 +0100

    fix(home-animation): when loading the sections it doesn't show the content due to race condition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import Aos from 'aos';
 import Brands from '../components/Brands';
 import Projects from '../components/Projects';
 // import Testimonial from '../components/Testimonial';
@@ -9,6 +11,10 @@ import Experience from '../components/Experience';
 import Contact from '../components/Contact';
 
 export default function Home() {
+    useEffect(() => {
+        Aos.refresh();
+    }, []);
+
     const {
         hero,
         socialBtns,
